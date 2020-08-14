5335d025c1a9b18e3a2d713e14620f9eb987fb8f
vikas <presentisgood@gmail.com>
2020-08-14 16:52:05 +0530

    Modified softlink generator to select based on user choices instead of cmd line params.

--- generate_softlinks.py
+++ generate_softlinks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import argparse
 import os
 import sys
+import glob
 
 def rm_file(f):
     if os.path.isfile(f) or os.path.islink(f):
@@ -10,18 +10,30 @@ def rm_file(f):
 # enddef
 
 if __name__ == '__main__':
-    parser  = argparse.ArgumentParser()
-    parser.add_argument('--tdir', help='Theme directory', type=str, default=None)
-    parser.add_argument('--json', help='Resume json file', type=str, default=None)
-    args    = parser.parse_args()
-
-    if args.__dict__['tdir'] is None or args.__dict__['json'] is None:
-        print('All arguments required !!')
+    resume_files = glob.glob1('resumes', '*.json')
+    print('>> Select resume file.')
+    for indx_t, res_file_t in enumerate(resume_files):
+        print('{}. {}'.format(indx_t, res_file_t))
+    # endfor
+    res_file_no = int(input('>> Enter choice ? '))
+    if res_file_no not in range(0, len(resume_files)):
+        print('>> ERROR:: Choice should be between {} and {}'.format(0, len(resume_files)))
         sys.exit(-1)
     # endif
 
-    theme_dir    = os.path.expanduser(args.__dict__['tdir'])
-    resume_json  = os.path.expanduser(args.__dict__['json'])
+    theme_dirs = glob.glob1('themes', '*')
+    print('>> Select theme.')
+    for indx_t, tdir_t in enumerate(theme_dirs):
+        print('{}. {}'.format(indx_t, tdir_t))
+    # endfor
+    tdir_no = int(input('>> Enter choice ? '))
+    if tdir_no not in range(0, len(theme_dirs)):
+        print('>> ERROR:: Choice should be between {} and {}'.format(0, len(theme_dirs)))
+        sys.exit(-1)
+    # endif
+
+    theme_dir    = os.path.join('themes', theme_dirs[tdir_no])
+    resume_json  = os.path.join('resumes', resume_files[res_file_no])
 
     # Set index.js
     dst = 'index.js'
--- run_default.sh
+++
@@ -1,1 +0,0 @@
-python3 generate_softlinks.py --tdir themes/jsonresume-theme-spartan --json resumes/resume_compact.json
